4ece8b1b4d9342c4153d4652e8b423645af91799
unknown <gonzalezleonardoantonio056@gmail.com>
2024-03-22 16:45:25 -0300

    finished 1st time

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,22 +1,22 @@
-import RingOne from './assets/rings/anillo-oro-plata(1).jpg';
-import RingTwo from './assets/rings/anillo-oro-plata(2).jpg';
-import Grabado from './assets/rings/vale.jfif';
+import RingGold1 from './assets/rings/anillo-oro-plata(1).jpg';
+import RingGold2 from './assets/rings/anillo-oro-plata(2).jpg';
+import Bracelet1 from './assets/rings/brazalete(1).jfif';
 /*import RingTwo from './assets/rings/m-normal-in-gray(2).jpg';
 import RingThree from './assets/rings/m-bg-gray(3).jpg';
 import RingFour from './assets/rings/m-normal(4).jpg';
 import RingFive from './assets/rings/anillo-oro-plata-1(5).jpg';*/
-import RingSix from './assets/rings/snake (6).jpg';
-import RingSeven from './assets/rings/black (7).jpg';
-import RingEight from './assets/rings/black (8).jpg';
-import RingNine from './assets/rings/gold (9).jpg';
-import RingTen from  './assets/rings/gold (10).jpg';
-import RingEleven from './assets/rings/gold (11).jpg';
+import RingArtes1 from './assets/rings/anillo-artesanal(1).jpg';
+import RingArtes2 from './assets/rings/anillo-artesanal(2).jpg';
+import RingArtes3 from './assets/rings/anillo-artesanal(3).jpg';
+import RingNine from './assets/rings/anillo-grabado-espejo(1).jpg';
+import RingTen from  './assets/rings/anillo-grabado-espejo(2).jpg';
+import RingEleven from './assets/rings/anillo-grabado-espejo(3).jpg';
 
 
-const images = [RingOne, RingTwo, Grabado,
-                RingSix ,RingSeven,RingEight,RingNine,RingTen,RingEleven,];
+const images = [RingArtes1 ,RingArtes2, RingArtes3,RingNine,RingTen,RingEleven,
+                RingGold1, RingGold2 , Bracelet1];
 
-const name = ["ANILLO DE LETRA", " ANILLO DE LETRA" ,"BRAZALETES","ANILLOS ARTESANALES","ANILLOS ARTESANALES","ANILLOS ARTESANALES"," AN. GRABADO ESPEJO","AN. GRABADO ESPEJO","AN. GRABADO ESPEJO",]
+const name = ["ANILLOS ARTESANALES","ANILLOS ARTESANALES","ANILLOS ARTESANALES"," AN. GRABADO ESPEJO","AN. GRABADO ESPEJO","AN. GRABADO ESPEJO","ANILLO DE LETRA", " ANILLO DE LETRA" ,"BRAZALETES"]
 /*Eliminar del 2-4 */
 
 export default function News() {
@@ -31,9 +31,9 @@ export default function News() {
             <div className='pb-10'>
             <div className='inline-flex flex-wrap justify-center gap-8'>
                 {images.map((img, index) => (
-                    <figure className='inline-block min-w-13 min-h-13 max-w-[100px] max-h-[100px] mt-4' key={index}>
+                    <figure className='inline-block min-w-13 min-h-13 max-w-[100px] max-h-[100px] mt-8' key={index}>
                         <img className="w-full h-full rounded-md" src={img} alt="" />
-                        <figcaption className='inline text-white text-sm font-ibmplex'>{name[index]}</figcaption>
+                        <figcaption className='inline text-gray-200 text-sm font-ibmplex'>{name[index]}</figcaption>
                     </figure>
                 ))}
             </div>
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
                     <h2 className="text-black text-center text-xl py-5">Sobre Nosotros</h2>
                     <p className="text-base px-5 py-5">Somos un negocio familiar con más de 40 años de experiencia en el fascinante mundo de la 
                     orfebrería y los grabados. Nuestra dedicación se refleja en el excelente servicio que brindamos a nuestros clientes, 
-                    confían en nuestra artesanía y atención personalizada. Descubre la calidad y el cuidado en cada detalle en nuestros productos. 
+                    confían en nuestra artesanía y atención personalizada. Descubre la calidad y el cuidado en cada detalle de nuestros servicios. 
                     ¡Te invitamos a vivir la experiencia única que solo nosotros podemos ofrecerte!</p>
                 </section>
                 <section >
